9972a576fc9c99e2f78c508afb4ef1c4cea297ae
FelipeAzevedo <felipelemosaz@gmail.com>
2025-05-07 20:15:12 -0300

    change auth

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import authConfig from "../../config/auth";
+import authConfig from '../../config/auth';
 
 function authMiddleware(request, response, next) {
   const authToken = request.headers.authorization;
@@ -8,22 +8,21 @@ function authMiddleware(request, response, next) {
     return response.status(401).json({ error: "Token not provided" });
   }
 
-  const token = authToken.split(" ").at(1);
 
-  try {
-    jwt.verify(token, authConfig.secret, (err, decoded) => {
-      if (err) {
-        throw new Error();
-      }
+  const token = authToken.split(" ")[1];
 
-      request.userId = decoded.id;
-      request.userName = decoded.name;
-    });
-  } catch {
-    return response.status(401).json({ error: "Token is invalid" });
-  }
+  jwt.verify(token, authConfig.secret, (err, decoded) => {
+    if (err) {
+      return response.status(401).json({ error: "Token is invalid" });
+    }
 
-  return next();
+
+    request.userId = decoded.id;
+    request.userName = decoded.name;
+
+
+    next();
+  });
 }
 
 export default authMiddleware;

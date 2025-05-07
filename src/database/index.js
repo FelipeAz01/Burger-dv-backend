7dbea17a60c14ee6686886363f614eba5de2d151
FelipeAzevedo <felipelemosaz@gmail.com>
2025-05-07 19:57:15 -0300

    chance models import

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,9 @@
 import { Sequelize } from "sequelize";
 import mongoose from "mongoose";
 import configDatabase from "../config/database.js";
+import User from "./../app/models/User.js";
 
-import User from "../app/models/User";
+
 import Product from "../app/models/Product";
 import Category from "../app/models/category";
 

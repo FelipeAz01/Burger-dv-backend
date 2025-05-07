5e6df5e8a4be894e28dd362ef3a1e29168d543e5
FelipeAzevedo <felipelemosaz@gmail.com>
2025-05-07 20:08:03 -0300

    cat

--- src/database/index.js
+++ src/database/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import configDatabase from "../config/database.js";
 import User from "./../app/models/User.js";
 import Product from "./../app/models/Product.js";
-import Category from "../app/models/category";
+import Category from "../app/models/category.js";
 
 const models = [User, Product, Category];
 
